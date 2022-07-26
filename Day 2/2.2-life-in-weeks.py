ed3b40ef42f45395ac3ec13788f932331e4fabb0
Gibran Kaleel <gibran.kaleel.286@gmail.com>
2022-07-26 14:28:28 +0530

    Update exercise.

---
+++ Day 2/2.2-life-in-weeks.py
@@ -0,0 +1,14 @@
+# Get current age.
+age = int(input("What is your current age? "))
+
+# Number of years left.
+years_left = 90 - age
+
+# How many days, weeks, and months remain from a 90 year life. (1 Year = 365 Days, 52 Weeks, 12 Months)
+days = years_left * 365
+weeks = years_left * 52
+months = years_left * 12
+
+# Output message.
+message = (f"If you made it to 90, you have {days} days, {weeks} weeks, and {months} months left.")
+print(message)
