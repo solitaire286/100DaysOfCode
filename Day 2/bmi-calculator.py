7719bd4ba61ab591525c9632da2da793da0aa9cd
Gibran Kaleel <gibran.kaleel.286@gmail.com>
2022-07-26 14:27:47 +0530

    Rename file.

--- Day 2/bmi-calculator.py
+++
@@ -1,9 +0,0 @@
-# Get height and weight, and convert into numerical value.
-height = float(input("enter your height in m: "))
-weight = int(input("enter your weight in kg: "))
-
-# Calculate BMI and output as an integer value.
-bmi = int(weight / (height ** 2))
-
-# Print BMI.
-print(bmi)
