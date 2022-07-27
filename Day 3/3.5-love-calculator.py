5b9718a342d4c5495383fcf9308a3497535edcee
Gibran Kaleel <gibran.kaleel.286@gmail.com>
2022-07-27 19:39:55 +0530

    Update exercise.

--- Day 3/3.5-love-calculator.py
+++ Day 3/3.5-love-calculator.py
@@ -5,15 +5,15 @@ print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 
-# Combine both names into a single string and convert to lower case for use with the count() method.
-lower_case_names = (name1 + " " + name2).lower()
+# Concatenate both names into a single string and convert to lower case for use with the count() method.
+joint_names = (name1 + " " + name2).lower()
 
-# Count the number of times the letters, TRUE and LOVE occur in the combined names.
-count_true_in_names = lower_case_names.count('t') + lower_case_names.count('r') + lower_case_names.count('u') + lower_case_names.count('e')
-count_love_in_names = lower_case_names.count('l') + lower_case_names.count('o') + lower_case_names.count('v') + lower_case_names.count('e')
+# Count the number of times the letters, TRUE and LOVE occur in the combined names string.
+count_true = joint_names.count('t') + joint_names.count('r') + joint_names.count('u') + joint_names.count('e')
+count_love = joint_names.count('l') + joint_names.count('o') + joint_names.count('v') + joint_names.count('e')
 
 # Calculate the combined love score.
-love_score = int(str(count_true_in_names) + str(count_love_in_names))
+love_score = int(str(count_true) + str(count_love))
 
 # Output message based on combined love score.
 if love_score <= 10 or love_score >= 90:
