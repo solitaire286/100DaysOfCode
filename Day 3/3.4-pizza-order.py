e880377db2552f8ebb13bb696e8d35b1e0627c68
Gibran Kaleel <gibran.kaleel.286@gmail.com>
2022-07-27 14:51:03 +0530

    Update exercise.

--- Day 3/3.4-pizza-order.py
+++ Day 3/3.4-pizza-order.py
@@ -1,40 +1,58 @@
 # Greeting.
 print("Welcome to Python Pizza Deliveries!")
 
-# Get size of pizza.
+# Get pizza size.
 size = input("What size pizza do you want? S, M, or L ")
 
-# Price of pizza based on size. $15 (S), $20 (M), $25 (L)
-# Price of extra toppings based on size. Pepperoni: +$2 (S), +3$ (M/L) // Cheese: $1 (Any)
+# Get extra toppings.
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+# Price of pizza. $15 (S), $20 (M), $25 (L)
+# Price of extra toppings. Pepperoni: +$2 (S), +3$ (M/L) // Cheese: $1 (Any)
 
 # If small pizza.
 if size == "S":
   bill = 15
-  add_pepperoni = input("Do you want pepperoni? Y or N ")
   if add_pepperoni == "Y":
     bill += 2
-  extra_cheese = input("Do you want extra cheese? Y or N ")
   if extra_cheese == "Y":
     bill += 1
-  print(f"Your final bill is: ${bill}")
+  print(f"Your final bill is: ${bill}.")
 # If medium pizza.
 elif size == "M":
   bill = 20
-  add_pepperoni = input("Do you want pepperoni? Y or N ")
   if add_pepperoni == "Y":
     bill += 3
-  extra_cheese = input("Do you want extra cheese? Y or N ")
   if extra_cheese == "Y":
     bill += 1
-  print(f"Your final bill is: ${bill}")
+  print(f"Your final bill is: ${bill}.")
 # If large pizza.
 elif size == "L":
-  bill = 20
-  add_pepperoni = input("Do you want pepperoni? Y or N ")
+  bill = 25
   if add_pepperoni == "Y":
     bill += 3
-  extra_cheese = input("Do you want extra cheese? Y or N ")
   if extra_cheese == "Y":
     bill += 1
-  print(f"Your final bill is: ${bill}")
-  
+  print(f"Your final bill is: ${bill}.")
+
+# Angela's Solution.
+# bill = 0
+
+# if size == "S":
+#   bill += 15
+# elif size == "M":
+#   bill += 20
+# else:
+#   bill += 25
+
+# if add_pepperoni == "Y":
+#   if size == "S":
+#     bill += 2
+#   else:
+#     bill += 3
+
+# if extra_cheese == "Y":
+#     bill += 1
+
+# print(f"Your final bill is: ${bill}.")
