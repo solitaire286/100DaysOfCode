683c299a0dec31842860ff6f50a211c46f7850de
Gibran Kaleel <gibran.kaleel.286@gmail.com>
2022-07-28 20:45:43 +0530

    Update exercise.

--- Day 3/3.6-treasure-island.py
+++ Day 3/3.6-treasure-island.py
@@ -31,7 +31,7 @@ print('''
 
 print("Your mission is to find the treasure. Good Luck!\n")
 
-left_or_right = (input("A little ways into your quest, you are at a crossroads. Surrounded by a dense fog, you cannot see far. Which way do you want to go? " + 'Type "left" or "right"\n')).lower()
+left_or_right = (input("A little way into your quest, you are at a crossroads. Surrounded by a dense fog, you cannot see far. Which way do you want to go? " + 'Type "left" or "right" to get going.\n')).lower()
 
 if left_or_right == "left":
   
@@ -40,26 +40,26 @@ if left_or_right == "left":
   if wait_or_swim == "wait":
     doors = (input("Barely making it out alive across treacherous waters. You arrive at an eerie old hut with three doors. With the numbers I, II, and III scratched into the wood. Which will you go through?\n")).lower()
     
-    if doors == "I":
-      print("You walked into a cursed room! You immediately feel something stabbing in your gut. You start bleeding profusely through mouth and nose, unable to scream or drag yourself out of the room. You die a gruesome death in a pool of your own blood... Game Over! ")
+    if doors == "i":
+      print("You walked into a cursed room! You immediately feel something stabbing in your gut. You start bleeding profusely through your mouth and nose, unable to scream or drag yourself out of the room. You die a gruesome death in a pool of your own blood...\nGame Over!")
     
-    elif doors == "II":
+    elif doors == "ii":
       print("You found the treasure. Congratulations!!!")
     
-    elif doors == "III":
-      print("You walk into the room and look around for a moment... It seemed empty at first. But no, it was booby-trapped! You are impaled by a flurry of poisoned spears hurled your way... You did not survive. Game Over!")
+    elif doors == "iii":
+      print("You walk into the room and look around for a moment... It seemed empty at first. But no, it was booby-trapped! You are impaled by a flurry of poisoned spears hurled your way... You did not survive.\nGame Over!")
   
     else:
-      print("You took too long to get into the hut. The crocodile from the lake followed you ashore. It's too big and fast, and there's no where to run except back into the water. You can't escape. You're eaten alive... Game Over!")
+      print("You took too long to get into the hut. The crocodile from the lake followed you ashore. It's too big and fast, and there's no where to run except back into the water. You cannot escape. You're eaten alive...\nGame Over!")
       
   elif wait_or_swim == "swim":
-    print("Halfway through your swim towards the island, you are being pulled down by something powerful. You cannot swim away, you cannot fight back. It's a huge crocodile beneath the cold, dark water dragging you to your demise... Game Over!")
+    print("Halfway through your swim towards the island, you are being pulled down by something powerful. You cannot swim away, you cannot fight back. It's a huge crocodile beneath the cold, dark water dragging you to your demise...\nGame Over!")
 
   else:
-    print("Freeze to death")
+    print("You didn't take the boat and it's too late to turn back now. Its nighttime and its raining heavily. You have no shelter to stay warm and dry. Hypothermia sets in, you're struggling to breathe and you loose consciousness... You slowly freeze to death.\nGame Over!")
 
 elif left_or_right == "right":
-  print("You hear a pack of hungry wolves coming your way. You panic and start running, while looking behind and not what is ahead. Suddenly, you are tumbling down a cliff. You hit your head on the way down and you die instantly... Game Over!")
+  print("You hear a pack of hungry wolves coming your way. You panic and start running, while looking behind and not what lies ahead. Suddenly, you are tumbling down a cliff. You hit your head on the way down and you die instantly...\nGame Over!")
 
 else:
-  print("You lingered too long at the crossroads. A pack of hungry wolves have caught up to you. Trying to fend them off, you succumb to your wounds and die a slow and painful death... Game Over!")
+  print("You lingered too long at the crossroads. A pack of hungry wolves have caught up to you. Trying to fend them off, you succumb to your wounds and die a slow and painful death...\nGame Over!")
