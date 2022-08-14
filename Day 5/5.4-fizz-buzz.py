b6c4ab5c4cea88f1d49a4f45d8193eeacbe510fc
Gibran Kaleel <gibran.kaleel.286@gmail.com>
2022-08-14 23:26:53 +0530

    Update exercise.

---
+++ Day 5/5.4-fizz-buzz.py
@@ -0,0 +1,13 @@
+# Print each number from 1 to 100 in turn.
+for number in range(1, 101):
+  # If number is divisible by 3 and 5, print "FizzBuzz".
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  # If number is divisible by 3, print "Fizz".
+  elif number % 3 == 0:
+    print("Fizz")
+  # If number is divisible by 5, print "Buzz".
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
