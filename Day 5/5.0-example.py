10e82c5ee987a503f435e0ce519840efd05cf183
Gibran Kaleel <gibran.kaleel.286@gmail.com>
2022-08-14 17:10:54 +0530

    Update exercise.

---
+++ Day 5/5.0-example.py
@@ -0,0 +1,11 @@
+#'for' loops iterate over a given sequence.
+
+brands = ["Apple", "Samsung", "Xiaomi"]
+for brand in brands:
+  print(brand)
+
+# sum()' function returns a number, the sum of all items in an iterable.
+
+count = (1, 2, 3, 4, 5)
+total = sum(count)
+print(total)
