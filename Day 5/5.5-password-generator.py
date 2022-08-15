9440c9f34ecf76a02e7b49c2e04d16108dc2d597
Gibran Kaleel <gibran.kaleel.286@gmail.com>
2022-08-15 16:29:10 +0530

    Update exercise.

--- Day 5/5.5-password-generator.py
+++ Day 5/5.5-password-generator.py
@@ -6,8 +6,8 @@ symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input("How many symbols would you like?\n"))
+nr_numbers = int(input("How many numbers would you like?\n"))
 
 # Randomly pick characters from list(s) based on user input.
 rnd_letters = random.sample(letters, nr_letters)
