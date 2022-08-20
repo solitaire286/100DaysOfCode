af28af5fa76350a2c8f24dde62f492d91180efd6
Gibran Kaleel <gibran.kaleel.286@gmail.com>
2022-08-20 13:51:05 +0530

    Update exercise.

--- Day 6/6.5-reeborgs-world-maze.py.py
+++ Day 6/6.5-reeborgs-world-maze.py.py
@@ -1,21 +1,29 @@
+from shutil import move
+
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
-def jump():
-    turn_left()
-    while wall_on_right():
-        move()
-    turn_right()
-    move()
-    turn_right()
-    while front_is_clear():
-        move()
-    turn_left()
-
-while not at_goal():
-    if wall_in_front():
-        jump()
+def find_path():
+    if wall_in_front() and right_is_clear():
+        turn_right()
+    elif wall_in_front() and wall_on_right():
+        turn_left()
+    elif wall_in_front():
+        turn_left()
     else:
         move()
+
+while not at_goal():
+    find_path()
+
+# Angela's Solution:
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
